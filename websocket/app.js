6e6651be16eb8c2c2d58473200263b01b24f316f
David Olson <mugency@gmail.com>
2018-05-28 19:18:23 -0400

    init the websocket files

---
+++ websocket/app.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+
+    // Get references to elements on the page.
+    let form = document.getElementById('message-form');
+    let messageField = document.getElementById('message');
+    let messagesList = document.getElementById('messages');
+    let socketStatus = document.getElementById('status');
+    let closeBtn = document.getElementById('close');
+
+    // The rest of the code in this tutorial will go here...
+
+};
